e1575c4f58295fb33a124f3c6990bdc4c4cc7f0d
mjfalahee <mjfalahee@gmail.com>
2019-06-17 13:12:15 -0700

    Added a series of tests for the repair method as a place to start

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,6 +5,16 @@ module.exports = {
   get,
 };
 
+//items = { name, durability, enhancement }
+//enhancement : 0 <= 20
+//durability : 0 <= 100
+
+//enhance succeeds, enhancement++. If enhancement = 20, the level isn't changed. Durability is unchanged
+
+//enhancement fails: if < 15, the durability decreases by 5.
+//if the enhancement >= 15, the durability -= 10
+//if the enhancement > 16, the enhancement level --
+
 function succeed(item) {
   return { ...item };
 }
@@ -14,7 +24,7 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+//items = { name, durability, enhancement }
+//enhancement : 0 <= 20
+//durability : 0 <= 100
+
+//enhance succeeds, enhancement++. If enhancement = 20, the level isn't changed. Durability is unchanged
+
+//enhancement fails: if < 15, the durability decreases by 5.
+//if the enhancement >= 15, the durability -= 10
+//if the enhancement > 16, the enhancement level --
+
+
+describe('enhancer.js', () => {
+    describe('enhancer.repair()', () => {
+        it('restores durability to 100', () => {
+            expect(enhancer.repair({durability: 50}).durability).toBe(100);
+            expect(enhancer.repair({durability: -50}).durability).toBe(100);
+            expect(enhancer.repair({durability: 100}).durability).toBe(100);
+            expect(enhancer.repair({durability: 0})).toEqual({durability: 100});
+        })
+    })
+})
